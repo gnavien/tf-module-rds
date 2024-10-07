c3c05c6e3e595c8e79cb5b1b6b26e62769718c0c
gnavien <gnavien@gmail.com>
2024-10-07 18:40:17 +0530

    update

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "component" {}
+variable "env" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "database_name" {}
+variable "subnet_ids" {}
+variable "tags" {}
+
+
+variable "db_name" {}
+#variable "kms_key_arn" {}
+#variable "instance_count" {}
+#variable "instance_class" {}
+variable "vpc_id" {}
+variable "sg_subnet_cidr" {}
+variable "instance_count" {}
+variable "instance_class" {}
