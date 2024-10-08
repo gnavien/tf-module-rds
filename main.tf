25c559ebb193c8013aa7ad984f020c6cf3aeb3d4
gnavien <gnavien@gmail.com>
2024-10-08 07:03:13 +0530

    update

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "main" {
 # In RDS we have cluster configuration and instance configuration
 
 resource "aws_db_subnet_group" "main" {
-  name       = "main"
+  name       = "${var.component}-${var.env}"
   subnet_ids = var.subnet_ids
   tags       = merge({ Name = "${var.component}-${var.env}" }, var.tags)
 }
